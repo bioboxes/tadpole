5c00cc0a40ab856f60d005abd12250362cb12b1a
Michael Barton <mail@michaelbarton.me.uk>
2016-08-11 16:48:25 -0700

    Update range of kmers tested for tadpole

--- image/bin/estimate_kmer.sh
+++ image/bin/estimate_kmer.sh
@@ -6,7 +6,7 @@ set -o xtrace
 READS=$1
 
 cd $(mktemp -d)
-echo $(tadwrapper.sh in=${READS} out=contigs_%.fa k=65 2>&1 \
+echo $(tadwrapper.sh in=${READS} out=contigs_%.fa k=31,62,93 2>&1 \
 	| grep "Recommended K:" \
 	| cut -f 2 -d ':' \
 	| tr -d "\t")
